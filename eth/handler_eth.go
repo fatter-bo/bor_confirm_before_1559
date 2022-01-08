2b6dc10cf4ffe78dfe6ac70e0978ad89b55e82cd
test <test@test.test>
2022-01-08 10:35:43 +0800

    不下载交易

--- eth/handler_eth.go
+++ eth/handler_eth.go
@@ -90,14 +90,16 @@ func (h *ethHandler) Handle(peer *eth.Peer, packet eth.Packet) error {
 	case *eth.NewBlockPacket:
 		return h.handleBlockBroadcast(peer, packet.Block, packet.TD)
 
-	case *eth.NewPooledTransactionHashesPacket:
-		return h.txFetcher.Notify(peer.ID(), *packet)
+		/*
+			case *eth.NewPooledTransactionHashesPacket:
+				return h.txFetcher.Notify(peer.ID(), *packet)
 
-	case *eth.TransactionsPacket:
-		return h.txFetcher.Enqueue(peer.ID(), *packet, false)
+			case *eth.TransactionsPacket:
+				return h.txFetcher.Enqueue(peer.ID(), *packet, false)
 
-	case *eth.PooledTransactionsPacket:
-		return h.txFetcher.Enqueue(peer.ID(), *packet, true)
+			case *eth.PooledTransactionsPacket:
+				return h.txFetcher.Enqueue(peer.ID(), *packet, true)
+				//*/
 
 	default:
 		return fmt.Errorf("unexpected eth packet type: %T", packet)
