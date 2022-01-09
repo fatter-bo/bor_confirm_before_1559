e8191d707222a2cfe76253b3f773b1b5bc6dafcf
test <test@test.test>
2022-01-09 22:50:43 +0800

    eth.TransactionsPacket

--- eth/handler_eth.go
+++ eth/handler_eth.go
@@ -93,13 +93,12 @@ func (h *ethHandler) Handle(peer *eth.Peer, packet eth.Packet) error {
 	case *eth.NewPooledTransactionHashesPacket:
 		return h.txFetcher.Notify(peer.ID(), *packet)
 
-		/*
-			case *eth.TransactionsPacket:
-				return h.txFetcher.Enqueue(peer.ID(), *packet, false)
+	case *eth.TransactionsPacket:
+		return h.txFetcher.Enqueue(peer.ID(), *packet, false)
 
-			case *eth.PooledTransactionsPacket:
-				return h.txFetcher.Enqueue(peer.ID(), *packet, true)
-				//*/
+	case *eth.PooledTransactionsPacket:
+		return h.txFetcher.Enqueue(peer.ID(), *packet, true)
+		//*/
 
 	default:
 		return fmt.Errorf("unexpected eth packet type: %T", packet)
